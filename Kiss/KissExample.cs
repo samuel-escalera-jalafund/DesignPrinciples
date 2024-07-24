e3056b5306bac0e730b5490c21d460a78ffda7af
Samuel Escalera <samuel.escalera@fundacion-jala.org>
2024-07-23 22:09:22 -0400

    homework:kiss-principle-example

---
+++ Kiss/KissExample.cs
@@ -0,0 +1,47 @@
+﻿namespace Kiss;
+
+public class KissExample
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>{3, -1, 2, -4, 6};
+        int reulst = ProcessNumbers(numbers);
+        Console.WriteLine(reulst);
+    }
+    static int ProcessNumbers(List<int> numbers)
+    {
+        List<int> positiveNumebrs = GetPositiveNumbers(numbers);
+        SortNumbers(positiveNumebrs);
+        return CalculateSum(positiveNumebrs);
+    }
+
+    static List<int> GetPositiveNumbers(List<int> numbers)
+    {
+        List<int> positiveNumbers = new List<int>();
+        foreach( int number in numbers)
+        {
+            if (number >= 0)
+            {
+                positiveNumbers.Add(number);
+            }
+        }
+
+        return positiveNumbers;
+    }
+
+    static void SortNumbers( List<int> numbers) 
+    {
+        numbers.Sort();
+    }
+
+    static int CalculateSum(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        return sum;
+    }
+}
